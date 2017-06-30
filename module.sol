8e3ba0d2e26d173d5cd53ae314af8cf5b42e22a1
Dexaran <dexaran820@gmail.com>
2017-06-30 13:04:20 +0400

    Update module.sol

--- module.sol
+++ module.sol
@@ -1,13 +1,13 @@
 pragma solidity ^0.4.11;
 
 contract abstractModuleHandler {
-    function balanceOf(address _owner) public constant returns (uint256 value, bool success) {}
-    function transferFrom(address _from, address _to, uint256 _value) external returns (bool) {}
+    function transfer(address from, address to, uint256 value, bool fee) external returns (bool success) {}
+    function balanceOf(address owner) public constant returns (bool success, uint256 value) {}
 }
 
 contract module {
     /*
-        Module handler
+        Module
     */
     
     enum status {
@@ -17,102 +17,126 @@ contract module {
         Disabled
     }
     
-    status private moduleStatus;
-    uint256 private disabledUntil;
-    address private moduleHandlerAddress;
+    status public moduleStatus;
+    uint256 public disabledUntil;
+    address public moduleHandlerAddress;
     
-    function _connectModule() internal returns (bool) {
-        /*
-            Registering and/or connecting-to ModuleHandler
-            
-            This function is called by ModuleHandler load.
-                or
-            Calls the Pool module at ModuleHandler
-        */
-        require( msg.sender == moduleHandlerAddress && moduleStatus == status.New );
-        moduleStatus = status.Connected;
-        moduleHandlerAddress = msg.sender;
+    function disableModule(bool _forever, uint256 _freezTime) external onlyForModuleHandler returns (bool success) {
+        _disableModule(_forever, _freezTime);
         return true;
     }
-    function _registerModuleHandler(address addr) internal returns(bool) {
-        /*
-            Registering ModuleHandler address
-            
-            This function is automatic called while depoying the contract.
-        */
-        require( moduleHandlerAddress == 0x00 && moduleStatus == status.New );
-        moduleHandlerAddress = addr;
-        return true;
-    }
-    function _disconnectModule() internal returns (bool) {
-        /*
-            Disconnect the module from the ModuleHandler
-            
-            This function calls the Poll module
-        */
-        require( msg.sender == moduleHandlerAddress && moduleStatus == status.Connected );
-        moduleStatus = status.Disconnected;
-        return true;
-    }
-    function _replaceModule(address addr) internal returns (bool) {
-        /*
-            Replace the module for an another new module
-            
-            This function calls the Poll module
-            
-            We send every Token and ether to the new module
-        */
-        require( msg.sender == moduleHandlerAddress && moduleStatus == status.Connected );
-        var (bal, s) = abstractModuleHandler(moduleHandlerAddress).balanceOf(address(this));
-        require( s );
-        if (bal > 0) {
-            abstractModuleHandler(moduleHandlerAddress).transferFrom(address(this), addr, bal);
-        }
-        if ( this.balance > 0 ) { if ( ! addr.send(this.balance) ) { return false; } }
-        delete moduleHandlerAddress;
-        moduleStatus = status.Disconnected;
-        return true;
-    }
-    function _isActive() internal returns (bool) {
-        /*
-            Ask for is active the module or not
-            
-            Free to call
-        */
-        if ( moduleStatus != status.Connected || block.number < disabledUntil ) { return false; }
-        return true;
-    }
-    function _disableModule(bool forever) internal returns (bool) {
+    function _disableModule(bool _forever, uint256 _freezTime) internal {
         /*
             Disable the module for one week, if the forever true then for forever.
+            This function calls the Publisher module.
             
-            This function calls the Poll module
+            @forever    For forever or not
         */
-        require( msg.sender == moduleHandlerAddress );
         if ( forever ) { moduleStatus = status.Disabled; }
-        disabledUntil = block.number + 40320;
+        disabledUntil = block.number + _freezTime;
+    }
+    
+    function replaceModuleHandler(address newModuleHandlerAddress) external onlyForModuleHandler returns (bool success) {
+        _replaceModuleHandler(newModuleHandlerAddress);
         return true;
     }
-    function _replaceModuleHandler(address newHandler) internal returns (bool) {
+    function _replaceModuleHandler(address newModuleHandlerAddress) internal {
         /*
             Replace the ModuleHandler address.
+            This function calls the Publisher module.
             
-            This function calls the Poll module
+            @newModuleHandlerAddress    New module handler address
         */
-        require( msg.sender == moduleHandlerAddress && moduleStatus == status.Connected );
-        moduleHandlerAddress = newHandler;
+        require( moduleStatus == status.Connected );
+        moduleHandlerAddress = newModuleHandlerAddress;
+    }
+    
+    function connectModule() external onlyForModuleHandler returns (bool success) {
+        _connectModule();
         return true;
     }
-    function _isModuleHandler(address addr) internal returns (bool) {
+    function _connectModule() internal {
         /*
-            Test for ModuleHandler address
+            Registering and/or connecting-to ModuleHandler.
+            This function is called by ModuleHandler load or by Publisher.
+        */
+        require( moduleStatus == status.New );
+        moduleStatus = status.Connected;
+    }
+    
+    function disconnectModule() external onlyForModuleHandler returns (bool success) {
+        _disconnectModule();
+        return true;
+    }
+    function _disconnectModule() internal {
+        /*
+            Disconnect the module from the ModuleHandler.
+            This function calls the Publisher module.
+        */
+        require( moduleStatus != status.New && moduleStatus != status.Disconnected );
+        moduleStatus = status.Disconnected;
+    }
+    
+    function replaceModule(address newModuleAddress) external onlyForModuleHandler returns (bool success) {
+        _replaceModule(newModuleAddress);
+        return true;
+    }
+    function _replaceModule(address newModuleAddress) internal {
+        /*
+            Replace the module for an another new module.
+            This function calls the Publisher module.
+            We send every Token and ether to the new module.
             
-            Free to call
+            @newModuleAddress   New module handler address
+        */
+        require( moduleStatus != status.New && moduleStatus != status.Disconnected);
+        var (_success, _balance) = abstractModuleHandler(moduleHandlerAddress).balanceOf(address(this));
+        require( _success );
+        if ( _balance > 0 ) {
+            require( abstractModuleHandler(moduleHandlerAddress).transfer(address(this), newModuleAddress, _balance, false) );
+        }
+        if ( this.balance > 0 ) {
+            require( newModuleAddress.send(this.balance) );
+        }
+        moduleStatus = status.Disconnected;
+    }
+    
+    function transferEvent(address from, address to, uint256 value) external onlyForModuleHandler returns (bool success) {
+        return true;
+    }
+    function newSchellingRoundEvent(uint256 roundID, uint256 reward) external onlyForModuleHandler returns (bool success) {
+        return true;
+    }
+    
+    function registerModuleHandler(address _moduleHandlerAddress) internal {
+        /*
+            Module constructor function for registering ModuleHandler address.
+        */
+        moduleHandlerAddress = _moduleHandlerAddress;
+    }
+    function isModuleHandler(address addr) internal returns (bool ret) {
+        /*
+            Test for ModuleHandler address.
+            If the module is not connected then returns always false.
+            
+            @addr   Address to check
+            
+            @ret    This is the module handler address or not
         */
         if ( moduleStatus != status.Connected ) { return false; }
         return addr == moduleHandlerAddress;
     }
-    function _getModuleHandlerAddress() internal returns (address) {
-        return moduleHandlerAddress;
+    function isActive() public constant returns (bool success, bool active) {
+        /*
+            Check self for ready for functions or not.
+            
+            @success    Function call was successfull or not
+            @active     Ready for functions or not
+        */
+        return (true, moduleStatus == status.Connected && block.number >= disabledUntil);
+    }
+    modifier onlyForModuleHandler() {
+        require( msg.sender == moduleHandlerAddress );
+        _;
     }
 }
